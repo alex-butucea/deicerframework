21885a8091356459d37da09f9df8b29f518ee2e0
Alex Butucea <alex826@gmail.com>
2013-07-06 12:28:23 +0100

    Added Token Consumer interface

---
+++ library/Colt/Stdlib/TokenConsumerInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Colt\Stdlib;
+
+/**
+ * Interface for objects that consume a uniquely identifiable token
+ *
+ * @category   Colt
+ * @package    Stdlib
+ * @version    $id$
+ * @copyright  2013 Alex Butucea <alex826@gmail.com>
+ * @author     Alex Butucea <alex826@gmail.com>
+ * @license    The MIT License (MIT) {@link http://opensource.org/licenses/MIT}
+ */
+interface TokenConsumerInterface
+{
+    /**
+     * Set unique token
+     *
+     * @param  mixed $token The token to set
+     * @return void
+     */
+    public function setToken($token);
+}
