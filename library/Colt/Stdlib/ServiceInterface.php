f6065288c5d640e1d42ae38a3e2aafc64427965e
Alex Butucea <alex826@gmail.com>
2013-07-06 02:27:59 +0100

    Added Service interface

---
+++ library/Colt/Stdlib/ServiceInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Colt\Stdlib;
+
+/**
+ * Marker interface for objects that provide a service to consuming code
+ *
+ * @category   Colt
+ * @package    Stdlib
+ * @version    $id$
+ * @copyright  2013 Alex Butucea <alex826@gmail.com>
+ * @author     Alex Butucea <alex826@gmail.com>
+ * @license    The MIT License (MIT) {@link http://opensource.org/licenses/MIT}
+ */
+interface ServiceInterface
+{
+}
