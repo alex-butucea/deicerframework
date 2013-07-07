2d209959d20611d2ac6583d7055b7c94e1d52101
Alex Butucea <alex826@gmail.com>
2013-07-07 01:38:09 +0100

    Added Pubsub Event interface

---
+++ library/Colt/Stdlib/Pubsub/EventInterface.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Colt\Stdlib\Pubsub;
+
+/**
+ * Representation of an event raised by a publisher object
+ *
+ * @category   Colt
+ * @package    Stdlib
+ * @subpackage Pubsub
+ * @version    $id$
+ * @copyright  2013 Alex Butucea <alex826@gmail.com>
+ * @author     Alex Butucea <alex826@gmail.com>
+ * @license    The MIT License (MIT) {@link http://opensource.org/licenses/MIT}
+ */
+interface EventInterface
+{
+    /**
+     * Get the event topic
+     *
+     * @return string
+     */
+    public function getTopic();
+
+    /**
+     * Get the content payload
+     *
+     * @return string
+     */
+    public function getContent();
+
+    /**
+     * Get the event publisher
+     *
+     * @return string
+     */
+    public function getPublisher();
+}
