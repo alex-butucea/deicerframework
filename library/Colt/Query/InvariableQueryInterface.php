9c0efadaf739e13017ff01775fb950f4e7825358
Alex Butucea <alex826@gmail.com>
2013-07-08 21:09:56 +0100

    Added query marker interfaces

---
+++ library/Colt/Query/InvariableQueryInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Colt\Query;
+
+/**
+ * Marker interface for invariable queries
+ *
+ * @category   Colt
+ * @package    Query
+ * @version    $id$
+ * @copyright  2013 Alex Butucea <alex826@gmail.com>
+ * @author     Alex Butucea <alex826@gmail.com>
+ * @license    The MIT License (MIT) {@link http://opensource.org/licenses/MIT}
+ */
+interface InvariableQueryInterface
+{
+}
