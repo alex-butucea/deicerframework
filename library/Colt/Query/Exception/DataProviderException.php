167fabe28e24ccd051f596c7b55dfdc0bf91de13
Alex Butucea <alex826@gmail.com>
2013-07-19 02:01:32 +0100

    Added Query Data Provider Exception

---
+++ library/Colt/Query/Exception/DataProviderException.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Colt\Query\Exception;
+
+use Colt\Query\Exception\ExceptionInterface;
+
+/**
+ * Colt Query Data Provider Exception
+ *
+ * Represents the failure of a Query implementation to provide valid data
+ *
+ * @category   Colt
+ * @package    Query
+ * @subpackage Exception
+ * @version    $id$
+ * @copyright  2013 Alex Butucea <alex826@gmail.com>
+ * @author     Alex Butucea <alex826@gmail.com> 
+ * @license    The MIT License (MIT) {@link http://opensource.org/licenses/MIT}
+ */
+class DataProviderException implements ExceptionInterface
+{
+}
