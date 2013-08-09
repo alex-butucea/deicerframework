f647510a327b1df2ae168683a034d8636e39a818
Alex Butucea <alex826@gmail.com>
2013-08-09 01:15:45 +0100

    Tweaked implementation hinting in Parameter Consumer Interface docblock

--- library/Deicer/Stdlib/ParameterConsumerInterface.php
+++ library/Deicer/Stdlib/ParameterConsumerInterface.php
@@ -25,6 +25,7 @@ interface ParameterConsumerInterface
     /**
      * Set a single parameter
      *
+     * @throws NonStringException If $name is not a string
      * @param  string $name  The name of the parameter to set
      * @param  mixed  $value The value to set
      * @return void
