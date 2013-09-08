ff073ed78bf260639ab5eabfe876541c32d4abe2
Alex Butucea <alex826@gmail.com>
2013-09-08 01:11:46 +0100

    Inserted page-level docblock

--- library/Deicer/Pubsub/MessageBuilderInterface.php
+++ library/Deicer/Pubsub/MessageBuilderInterface.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Deicer Framework (http://github.com/alex-butucea/deicerframework)
+ *
+ * @link       http://github.com/alex-butucea/deicerframework for canonical source repository
+ * @copyright  2013 Alex Butucea <alex826@gmail.com>
+ * @license    The MIT License (MIT) {@link http://opensource.org/licenses/MIT}
+ */
 
 namespace Deicer\Pubsub;
 
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * Deicer Test Bootstrap
+ * Deicer Framework (http://github.com/alex-butucea/deicerframework)
  *
- * @version    $id$
+ * @link       http://github.com/alex-butucea/deicerframework for canonical source repository
  * @copyright  2013 Alex Butucea <alex826@gmail.com>
- * @author     Alex Butucea <alex826@gmail.com> 
  * @license    The MIT License (MIT) {@link http://opensource.org/licenses/MIT}
  */
 
