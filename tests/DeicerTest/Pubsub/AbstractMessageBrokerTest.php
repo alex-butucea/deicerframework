54aff1a081dc52e5d8371cd79821d25fc7c8ae2e
Alex Butucea <alex826@gmail.com>
2013-09-18 19:54:28 +0100

    Extracted common Message Broker Tests to abstract test case

---
+++ tests/DeicerTest/Pubsub/AbstractMessageBrokerTest.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * Deicer Framework (http://github.com/alex-butucea/deicerframework)
+ *
+ * @link       http://github.com/alex-butucea/deicerframework for canonical source repository
+ * @copyright  2013 Alex Butucea <alex826@gmail.com>
+ * @license    The MIT License (MIT) {@link http://opensource.org/licenses/MIT}
+ */
+
+namespace DeicerTest\Pubsub;
+
+/**
+ * Common Message Broker tests
+ *
+ * @category   DeicerTest
+ * @package    Pubsub
+ * @version    $id$
+ * @copyright  2013 Alex Butucea <alex826@gmail.com>
+ * @author     Alex Butucea <alex826@gmail.com>
+ * @license    The MIT License (MIT) {@link http://opensource.org/licenses/MIT}
+ */
+abstract class AbstractMessageBrokerTest extends \PHPUnit_Framework_TestCase
+{
+    public $fixture;
+    public $message;
+    public $publisher;
+    public $subscriber;
+    public $subscribers;
+
+    public function setUp()
+    {
+        $this->message     = $this->getMock('Deicer\Pubsub\MessageInterface');
+        $this->publisher   = $this->getMock('Deicer\Pubsub\PublisherInterface');
+        $this->subscriber  = $this->getMock('Deicer\Pubsub\SubscriberInterface');
+        $this->subscribers = array (
+            $this->getMock('Deicer\Pubsub\SubscriberInterface'),
+            $this->getMock('Deicer\Pubsub\SubscriberInterface'),
+            $this->getMock('Deicer\Pubsub\SubscriberInterface'),
+        );
+
+        $this->message
+            ->expects($this->any())
+            ->method('getTopic')
+            ->will($this->returnValue('foobar'));
+    }
+
+    public function testAddSubscribersWithNonSubscriberInterfaceThrowsException()
+    {
+        $this->setExpectedException('InvalidArgumentException');
+        $this->fixture->addSubscribers(
+            array (
+                $this->subscribers[0],
+                'foo',
+                $this->subscribers[2],
+            )
+        );
+    }
+
+    public function testAddSubscribersWithEmptyArrayThrowsException()
+    {
+        $this->setExpectedException('LengthException');
+        $this->fixture->addSubscribers(array ());
+    }
+
+    public function testAddSubscribersPopulatesSubscriberPool()
+    {
+        $this->fixture->addSubscribers($this->subscribers);
+        $this->assertSame(3, $this->fixture->addSubscriber($this->subscriber));
+    }
+
+    public function testAddSubscribersReturnsSubscribersReIndexed()
+    {
+        $actual = $this->fixture->addSubscribers($this->subscribers);
+
+        $this->assertSame($this->subscribers[0], $actual[0]);
+        $this->assertSame($this->subscribers[1], $actual[1]);
+        $this->assertSame($this->subscribers[2], $actual[2]);
+    }
+
+    public function testAddSubscriberAutoIncrementsSubscriberIndex()
+    {
+        $this->assertSame(0, $this->fixture->addSubscriber($this->subscriber));
+        $this->assertSame(1, $this->fixture->addSubscriber($this->subscriber));
+        $this->assertSame(2, $this->fixture->addSubscriber($this->subscriber));
+    }
+
+    public function testAddSubscriberIndexAutoIncrementsFromHighestIndex()
+    {
+        $this->assertSame(0, $this->fixture->addSubscriber($this->subscriber));
+        $this->fixture->removeSubscriber(0);
+        $this->assertSame(0, $this->fixture->addSubscriber($this->subscriber));
+        $this->assertSame(1, $this->fixture->addSubscriber($this->subscriber));
+        $this->assertSame(2, $this->fixture->addSubscriber($this->subscriber));
+        $this->fixture->removeSubscriber(1);
+        $this->assertSame(3, $this->fixture->addSubscriber($this->subscriber));
+    }
+
+    public function testRemoveSubscriberImplementsFluentInterface()
+    {
+        $index  = $this->fixture->addSubscriber($this->subscriber);
+        $actual = $this->fixture->removeSubscriber($index);
+        $this->assertSame($actual, $this->fixture);
+    }
+
+    public function testRemoveSubscriberWithNonIntIndexThrowsException()
+    {
+        $this->setExpectedException('InvalidArgumentException');
+        $this->fixture->removeSubscriber('foo');
+    }
+
+    public function testRemoveSubscriberWithNonExistentIndexThrowsException()
+    {
+        $this->setExpectedException('OutOfRangeException');
+        $this->fixture->removeSubscriber(2);
+    }
+
+    public function testRemoveSubscriberEnsuresNoMessageDelivery()
+    {
+        $this->subscribers[0]->expects($this->once())->method('update')->with($this->message);
+        $this->subscribers[1]->expects($this->never())->method('update');
+        $this->subscribers[2]->expects($this->once())->method('update')->with($this->message);
+
+        $this->fixture->addSubscribers($this->subscribers);
+        $this->fixture->removeSubscriber(1);
+        $this->fixture->publish($this->message);
+    }
+
+    public function testPublishImplementsFluentInterface()
+    {
+        $actual = $this->fixture->publish($this->message);
+        $this->assertSame($actual, $this->fixture);
+    }
+}
--- tests/DeicerTest/Pubsub/UnfilteredMessageBrokerTest.php
+++ tests/DeicerTest/Pubsub/UnfilteredMessageBrokerTest.php
@@ -10,6 +10,7 @@
 namespace DeicerTest\Pubsub;
 
 use Deicer\Pubsub\UnfilteredMessageBroker;
+use DeicerTest\Pubsub\AbstractMessageBrokerTest;
 
 /**
  * Deicer Unfiltered Message Broker test suite
@@ -21,80 +22,12 @@ use Deicer\Pubsub\UnfilteredMessageBroker;
  * @author     Alex Butucea <alex826@gmail.com>
  * @license    The MIT License (MIT) {@link http://opensource.org/licenses/MIT}
  */
-class UnfilteredMessageBrokerTest extends \PHPUnit_Framework_TestCase
+class UnfilteredMessageBrokerTest extends AbstractMessageBrokerTest
 {
-    public $fixture;
-    public $message;
-    public $publisher;
-    public $subscriber;
-
     public function setUp()
     {
-        $this->fixture    = new UnfilteredMessageBroker();
-        $this->message    = $this->getMock('Deicer\Pubsub\MessageInterface');
-        $this->publisher  = $this->getMock('Deicer\Pubsub\PublisherInterface');
-        $this->subscriber = $this->getMock('Deicer\Pubsub\SubscriberInterface');
-    }
-
-    public function testAddSubscribersWithNonSubscriberInterfaceThrowsException()
-    {
-        $this->setExpectedException('InvalidArgumentException');
-        $this->fixture->addSubscribers(
-            array (
-                $this->getMock('Deicer\Pubsub\SubscriberInterface'),
-                'foo',
-                $this->getMock('Deicer\Pubsub\SubscriberInterface'),
-            )
-        );
-    }
-
-    public function testAddSubscribersWithEmptyArrayThrowsException()
-    {
-        $this->setExpectedException('LengthException');
-        $this->fixture->addSubscribers(array ());
-    }
-
-    public function testAddSubscribersPopulatesSubscriberPool()
-    {
-        $this->fixture->addSubscribers(
-            array (
-                $this->getMock('Deicer\Pubsub\SubscriberInterface'),
-                $this->getMock('Deicer\Pubsub\SubscriberInterface'),
-                $this->getMock('Deicer\Pubsub\SubscriberInterface'),
-            )
-        );
-        $this->assertSame(3, $this->fixture->addSubscriber($this->subscriber));
-    }
-
-    public function testAddSubscribersReturnsSubscribersReIndexed()
-    {
-        $foo = $this->getMock('Deicer\Pubsub\SubscriberInterface');
-        $bar = $this->getMock('Deicer\Pubsub\SubscriberInterface');
-        $baz = $this->getMock('Deicer\Pubsub\SubscriberInterface');
-
-        $actual = $this->fixture->addSubscribers(array ($foo, $bar, $baz));
-
-        $this->assertSame($foo, $actual[0]);
-        $this->assertSame($bar, $actual[1]);
-        $this->assertSame($baz, $actual[2]);
-    }
-
-    public function testAddSubscriberAutoIncrementsSubscriberIndex()
-    {
-        $this->assertSame(0, $this->fixture->addSubscriber($this->subscriber));
-        $this->assertSame(1, $this->fixture->addSubscriber($this->subscriber));
-        $this->assertSame(2, $this->fixture->addSubscriber($this->subscriber));
-    }
-
-    public function testAddSubscriberIndexAutoIncrementsFromHighestIndex()
-    {
-        $this->assertSame(0, $this->fixture->addSubscriber($this->subscriber));
-        $this->fixture->removeSubscriber(0);
-        $this->assertSame(0, $this->fixture->addSubscriber($this->subscriber));
-        $this->assertSame(1, $this->fixture->addSubscriber($this->subscriber));
-        $this->assertSame(2, $this->fixture->addSubscriber($this->subscriber));
-        $this->fixture->removeSubscriber(1);
-        $this->assertSame(3, $this->fixture->addSubscriber($this->subscriber));
+        $this->fixture = new UnfilteredMessageBroker();
+        parent::setUp();
     }
 
     public function testAddSubscriberEnsuresMessageDelivery()
@@ -108,46 +41,6 @@ class UnfilteredMessageBrokerTest extends \PHPUnit_Framework_TestCase
         $baz->expects($this->once())->method('update')->with($this->message);
 
         $this->fixture->addSubscribers(array ($foo, $bar, $baz));
-        $this->fixture->publish($this->message);
-    }
-
-    public function testRemoveSubscriberImplementsFluentInterface()
-    {
-        $index  = $this->fixture->addSubscriber($this->subscriber);
-        $actual = $this->fixture->removeSubscriber($index);
-        $this->assertSame($actual, $this->fixture);
-    }
-
-    public function testRemoveSubscriberWithNonIntIndexThrowsException()
-    {
-        $this->setExpectedException('InvalidArgumentException');
-        $this->fixture->removeSubscriber('foo');
-    }
-
-    public function testRemoveSubscriberWithNonExistentIndexThrowsException()
-    {
-        $this->setExpectedException('OutOfRangeException');
-        $this->fixture->removeSubscriber(2);
-    }
-
-    public function testPublishImplementsFluentInterface()
-    {
-        $actual = $this->fixture->publish($this->message);
-        $this->assertSame($actual, $this->fixture);
-    }
-
-    public function testRemoveSubscriberEnsuresNoMessageDelivery()
-    {
-        $foo = $this->getMock('Deicer\Pubsub\SubscriberInterface');
-        $bar = $this->getMock('Deicer\Pubsub\SubscriberInterface');
-        $baz = $this->getMock('Deicer\Pubsub\SubscriberInterface');
-
-        $foo->expects($this->once())->method('update')->with($this->message);
-        $bar->expects($this->never())->method('update');
-        $baz->expects($this->once())->method('update')->with($this->message);
-
-        $this->fixture->addSubscribers(array ($foo, $bar, $baz));
-        $this->fixture->removeSubscriber(1);
         $this->fixture->publish($this->message);
     }
 }
