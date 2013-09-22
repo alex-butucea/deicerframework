000aa32467f5537d23d52e642fe869313a2a59cc
Alex Butucea <alex826@gmail.com>
2013-09-23 00:13:36 +0100

    Added Unfiltered Pubsub Message Publisher interface

---
+++ library/Deicer/Pubsub/UnfilteredPublisherInterface.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Deicer Framework (http://github.com/alex-butucea/deicerframework)
+ *
+ * @link       http://github.com/alex-butucea/deicerframework for canonical source repository
+ * @copyright  2013 Alex Butucea <alex826@gmail.com>
+ * @license    The MIT License (MIT) {@link http://opensource.org/licenses/MIT}
+ */
+
+namespace Deicer\Pubsub;
+
+/**
+ * Unfiltered Pubsub Message Publisher
+ *
+ * Subscribers register themselves with broker to receive messages of all topics
+ *
+ * @category   Deicer
+ * @package    Pubsub
+ * @version    $id$
+ * @copyright  2013 Alex Butucea <alex826@gmail.com>
+ * @author     Alex Butucea <alex826@gmail.com>
+ * @license    The MIT License (MIT) {@link http://opensource.org/licenses/MIT}
+ */
+interface UnfilteredPublisherInterface
+{
+    /**
+     * Returns the unfiltered message broker for subscribers
+     * 
+     * @return UnfilteredMessageBrokerInterface
+     */
+    public function getUnfilteredMessageBroker();
+}
