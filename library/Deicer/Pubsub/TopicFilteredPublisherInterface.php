89482b9bba5bfe9c378ff8ec5de17840e6b868cc
Alex Butucea <alex826@gmail.com>
2013-09-23 00:13:59 +0100

    Added Topic Filtered Pubsub Message Publisher interface

---
+++ library/Deicer/Pubsub/TopicFilteredPublisherInterface.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Deicer Framework (http://github.com/alex-butucea/deicerframework)
+ *
+ * @link       http://github.com/alex-butucea/deicerframework for canonical source repository
+ * @copyright  2013 Alex Butucea <alex826@gmail.com>
+ * @license    The MIT License (MIT) {@link http://opensource.org/licenses/MIT}
+ */
+
+namespace Deicer\Pubsub;
+
+/**
+ * Topic Filtered Pubsub Message Publisher
+ *
+ * Subscribers register themselves with broker to receive messages of select topics
+ *
+ * @category   Deicer
+ * @package    Pubsub
+ * @version    $id$
+ * @copyright  2013 Alex Butucea <alex826@gmail.com>
+ * @author     Alex Butucea <alex826@gmail.com>
+ * @license    The MIT License (MIT) {@link http://opensource.org/licenses/MIT}
+ */
+interface TopicFilteredPublisherInterface
+{
+    /**
+     * Returns the topic filtered message broker for subscribers
+     * 
+     * @return TopicFilteredMessageBrokerInterface
+     */
+    public function getTopicFilteredMessageBroker();
+}
